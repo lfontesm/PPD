d5bee9aa4e83b5c9fe8195e3957f576b2f0e037d
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-07-25 18:28:53 -0300

    Um pequeno erro foi corrigido

--- t1.2/cliente/cliente.py
+++ t1.2/cliente/cliente.py
@@ -10,8 +10,9 @@ connection_ = xmlrpc.client.ServerProxy('http://localhost:8000')
 
 MAX_LEN = 100000
 
-arrNthreads_prod = [10]
-arrNthreads = [2, 4, 8]
+# Mudar o numero de threads a ser usada
+K = 2
+
 rng = np.random.default_rng(12345)
 
 def insert_numbers(len, connection):
@@ -23,13 +24,12 @@ def insert_numbers(len, connection):
 def retrieve_numbers(len, connection):
     for i in range(len):
         num = rng.integers(low=MAX_LEN, high=2*MAX_LEN)
-        # print(f'Cliente recuperou {connection.get(int(num))}')
+        connection.get(int(num))
 
 def interact_with_server(connection, threadNum):
     qtdRandNums = MAX_LEN//threadNum
 
     print(qtdRandNums, file=sys.stderr)
-    # print(f'My PID: {process.pid}')
     insert_numbers(qtdRandNums, connection)
     retrieve_numbers(qtdRandNums, connection)
 
@@ -37,17 +37,15 @@ def interact_with_server(connection, threadNum):
 start = time.time()
 jobs = []
 
-for i in arrNthreads_prod:
-    print(f'i = {i}')
-    for k in range(1, i + 1):
-        process = multiprocessing.Process(target=interact_with_server, args=(connection_, i))
-        jobs.append(process)
+for k in range(1, K + 1):
+    process = multiprocessing.Process(target=interact_with_server, args=(connection_, K))
+    jobs.append(process)
         
-    for thr in jobs:
-        thr.start()
+for thr in jobs:
+    thr.start()
 
-    for finishedThr in jobs:
-        finishedThr.join()
+for finishedThr in jobs:
+    finishedThr.join()
 
 print("Time eleapsed: " + str(time.time()-start))
 

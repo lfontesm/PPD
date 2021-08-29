92b5df3a3e4efad1246d5882537407dc0479402a
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-08-29 19:48:32 -0300

    :boom: Started the logic of the subscribers

--- t1.3/recv.py
+++ t1.3/recv.py
@@ -1,26 +1,60 @@
 #!/usr/bin/env python3
 
 import pika
+from multiprocessing import Process
+import random
+import time
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
+MAX_NUM=2**32
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+def boot():
+	time.sleep(5)
+	# gera num do id
+	nodeid=random.randint(0, MAX_NUM)
 
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
+	# conecta no rabbitmq
+	connection = pika.BlockingConnection(
+    	pika.ConnectionParameters(host='rabbitmq'))
+	channel = connection.channel()
 
-channel.queue_bind(exchange='logs', queue=queue_name)
+	channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
+	result = channel.queue_declare(queue='', exclusive=True)
+	queue_name = result.method.queue
+
+	channel.queue_bind(exchange='logs', queue=queue_name)
+
+	# msg q ele vai enviar pro broker
+	# no caso eu to tentando fazer o processo dele publicar o id do seu noh para o broker
+	message=f"Nodeid: {nodeid}"
+
+	# publica msg em todas as filas (routing_key='')
+	channel.basic_publish(exchange='logs', routing_key='', body=message)
+
+	#print(f'[*] {queue_name} is waiting for logs. To exit press CTRL+C')
+
+	channel.basic_consume(
+    	queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+	channel.start_consuming()
+
+	# pega msg da fila
+	#mf, hf, body = channel.basic_get(queue=queue_name, auto_ack=True)
+
+	# fecha conexao
+	connection.close()
 
 def callback(ch, method, properties, body):
-	log=open("messages.log", "a")
-	log.write(" [x] %r\n" % body)
-	log.close()
+	print(f"I  recieved", body)
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
+random.seed()
 
-channel.start_consuming()
+jobs=[]
+for i in range(8):
+	p=Process(target=boot)
+	jobs.append(p)
+for job in jobs:
+	job.start()
+for finishedjob in jobs:
+	finishedjob.join()
+

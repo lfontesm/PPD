4ee9924d577a141e056800958ed24d86b30358d3
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-08-30 18:09:43 -0300

    :poop: Added trash code that begind the logic of calculating the antecessor and successor of the nodes subscribed to the broker

--- t1.3/recv.py
+++ t1.3/recv.py
@@ -8,12 +8,13 @@ import time
 MAX_NUM=2**32
 
 def boot():
+	########## PROCEDIMENTO PARA QUE TODOS OS NOS ENVIEM SEUS RESPECTIVOS NODEID PARA O BROKER ##########
 	# gera num do id
 	nodeid=random.randint(0, MAX_NUM)
 
 	# msg q ele vai enviar pro broker
 	# no caso eu to tentando fazer o processo dele publicar o id do seu noh para o broker
-	message=f"Nodeid: {nodeid}"
+	message=f"{nodeid}"
 
 	# conecta no rabbitmq
 	connection = pika.BlockingConnection(
@@ -33,20 +34,62 @@ def boot():
 	channel.basic_publish(exchange='logs', routing_key='', body=message)
 	
 	time.sleep(2.5)
-	#print(f'[*] {queue_name} is waiting for logs. To exit press CTRL+C')
+	########## FIM. ABAIXO DESTA LINHA DEVE ESTAR O PROCEDIMENTO PARA OS NOS RECUPERAREM OS NODEID DA FILA DE MESSAGERIA #########
 
-	channel.basic_consume(
-    	queue=queue_name, on_message_callback=callback, auto_ack=True)
+	# Pega mensagens da fila ate esvaziar
+	nodeidlist=[]
+	while True:
+		mf,pr,body=channel.basic_get(queue=queue_name, auto_ack=True)
+		if mf:
+			nodeidlist.append(body)
+		else:
+			break
 
-	channel.start_consuming()
-
-	# pega msg da fila
-	#mf, hf, body = channel.basic_get(queue=queue_name, auto_ack=True)
-	#if mf:
-	#	print(body)
+	# Debug
+#	print('am here!')
 
 	# fecha conexao
 	connection.close()
+	return nodeid, nodeidlist
+
+# Funcao que faz o calculo para saber quais nos sao vizinhos do no atual.
+def calculate_neighbors():
+	nodeid, nodeidlist=boot()
+	
+	# Convert the elements in the list to int
+	int_nodeidlist=list( map(lambda x: int(x), nodeidlist) )
+
+	# Remove it's own nodeid from list
+	int_nodeidlist.remove(nodeid)
+
+	# print(f"{nodeid} -> {int_nodeidlist}")
+	# Subtract nodeid from the nodeid list to get the decimal distance from the current node
+	# Ex: The decimal distance between 1 and 9 is 8, bcz 9-1=8
+	distancelist=list( map(lambda x: x-nodeid, int_nodeidlist) )
+
+	sorted_distancelist=sorted(distancelist, key=abs)
+
+	upperbound=get_first_val(sorted_distancelist)
+	lowerbound=get_first_val(sorted_distancelist, positive=False)
+
+	print(upperbound, lowerbound)
+
+	#print(f"Index of upperbound in {nodeid}:", distancelist.index(upperbound))
+	#print(f"Index of lowerbound in {nodeid}:", distancelist.index(lowerbound))
+
+	#successor=int_nodeidlist[distancelist.index(upperbound)]
+	#predecessor=int_nodeidlist[distancelist.index(lowerbound)]
+
+	#print(f"My successor is {successor} and my predecessor is {predecessor}")
+
+
+def get_first_val(list_, positive=True):
+		for i in list_:
+			if positive and i>0:
+				return i
+			elif not positive and i<0:
+				return i
+
 
 def callback(ch, method, properties, body):
 	print(f"I  recieved", body)
@@ -55,7 +98,7 @@ random.seed()
 
 jobs=[]
 for i in range(8):
-	p=Process(target=boot)
+	p=Process(target=calculate_neighbors)
 	jobs.append(p)
 for job in jobs:
 	job.start()

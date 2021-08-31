5f0cd0a349cd524b6893ab44301ececa6f5a9d01
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-08-31 07:22:31 -0300

    :bug: Fixed the bug where the program would crash while trying to assign neighbors to the highest and lowest nodeid vals

--- t1.3/recv.py
+++ t1.3/recv.py
@@ -5,7 +5,7 @@ from multiprocessing import Process
 import random
 import time
 
-MAX_NUM=2**32
+MAX_NUM=2**10
 
 def boot():
 	########## PROCEDIMENTO PARA QUE TODOS OS NOS ENVIEM SEUS RESPECTIVOS NODEID PARA O BROKER ##########
@@ -59,29 +59,47 @@ def calculate_neighbors():
 	# Convert the elements in the list to int
 	int_nodeidlist=list( map(lambda x: int(x), nodeidlist) )
 
+	# Check if there are duplicates
+	if contains_duplicates(int_nodeidlist):
+		print("ABORTING: Duplicate nodes detected")
+
 	# Remove it's own nodeid from list
 	int_nodeidlist.remove(nodeid)
 
-	# print(f"{nodeid} -> {int_nodeidlist}")
+	if highest_between(nodeid, int_nodeidlist):
+		predecessor, successor = max(int_nodeidlist), min(int_nodeidlist)
+	elif lowest_between(nodeid, int_nodeidlist):
+		predecessor, successor = max(int_nodeidlist), min(int_nodeidlist)
+	else:
 	# Subtract nodeid from the nodeid list to get the decimal distance from the current node
 	# Ex: The decimal distance between 1 and 9 is 8, bcz 9-1=8
-	distancelist=list( map(lambda x: x-nodeid, int_nodeidlist) )
+		distancelist=list( map(lambda x: x-nodeid, int_nodeidlist) )
 
-	sorted_distancelist=sorted(distancelist, key=abs)
+		sorted_distancelist=sorted(distancelist, key=abs)
 
-	upperbound=get_first_val(sorted_distancelist)
-	lowerbound=get_first_val(sorted_distancelist, positive=False)
+		upperbound=get_first_val(sorted_distancelist)
+		lowerbound=get_first_val(sorted_distancelist, positive=False)
 
-	print(upperbound, lowerbound)
+		successor=int_nodeidlist[distancelist.index(upperbound)]
+		predecessor=int_nodeidlist[distancelist.index(lowerbound)]
 
-	#print(f"Index of upperbound in {nodeid}:", distancelist.index(upperbound))
-	#print(f"Index of lowerbound in {nodeid}:", distancelist.index(lowerbound))
+	print(f"I'm {nodeid} and my successor is {successor} and my predecessor is {predecessor}")
 
-	#successor=int_nodeidlist[distancelist.index(upperbound)]
-	#predecessor=int_nodeidlist[distancelist.index(lowerbound)]
+def highest_between(num, list_):
+	if num > max(list_):
+		return True
+	return False
 
-	#print(f"My successor is {successor} and my predecessor is {predecessor}")
+def lowest_between(num, list_):
+	if num < min(list_):
+		return True
+	return False
 
+def contains_duplicates(list_):
+	for elem in list_:
+		if list_.count(elem) > 1:
+			return True
+	return False
 
 def get_first_val(list_, positive=True):
 		for i in list_:
@@ -97,7 +115,7 @@ def callback(ch, method, properties, body):
 random.seed()
 
 jobs=[]
-for i in range(8):
+for i in range(4):
 	p=Process(target=calculate_neighbors)
 	jobs.append(p)
 for job in jobs:

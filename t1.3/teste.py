b716743781405d839ccb7b1209f8fe80393ded35
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-08-31 14:35:06 -0300

    :sparkles: Changed teste.py to include key when it sends message to teh broker

--- t1.3/teste.py
+++ t1.3/teste.py
@@ -9,7 +9,7 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-message=' '.join(sys.argv[1:]) or "info: Hello World"
+message='1,'+' '.join(sys.argv[1:]) or "info: Hello World"
 channel.basic_publish(exchange='logs', routing_key='', body=message)
 
 print("[x] Sent %r" % message)

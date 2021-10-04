937e8a4f2b7011e0eba59c77ada9db2ce88cbee6
elcineyjr <elcineyjr@gmail.com>
2021-10-04 20:13:45 -0300

    :recycle: refactoring how the node joins the dht

--- t2/dht.py
+++ t2/dht.py
@@ -14,6 +14,8 @@ NODEID_LIST = []
 PREDECESSOR = 1
 AM_CHILD = False
 
+####### Helper methods to handle the nodes list #######
+
 def is_outermost_val(num, list_):
     return num > max(list_) or num < min(list_)
 
@@ -37,16 +39,16 @@ def boundaries(list_, nodeid):
 
     return distance_list.index(lowerbound), distance_list.index(upperbound)
 
-# Calculate responsibility interval
-def accept_join(ch, method):
-    ch.stop_consuming()
-    queue_name = QUEUE_NAME
 
+####### DHT logic #######
 
+# Publishes the join message, calculates the node neighbors and defines the callback
+def prepare_join(channel, queue_name):
+    # Join message
     message = f"{NODEID}"
 
     # publishes the message to all queues (routing_key='')
-    ch.basic_publish(exchange='logs', routing_key='', body=message)
+    channel.basic_publish(exchange='logs', routing_key='', body=message)
     
     time.sleep(1)
 
@@ -55,43 +57,47 @@ def accept_join(ch, method):
     # Get all messages until the queue is empty
     nodeidlist=[]
     while True:
-        mf,pr,body=ch.basic_get(queue=queue_name, auto_ack=True)
+        mf,pr,body=channel.basic_get(queue=queue_name, auto_ack=True)
         if mf:
-            print(f"{NODEID} read from it's queue -> {body}")
             nodeidlist.append(body)
         else:
             break
-    
+
     # Convert the elements in the list to int
     int_nodeidlist=list( map(lambda x: int(x), nodeidlist) )
 
     # Remove it's own nodeid from list
-    print(NODEID, 'and', int_nodeidlist)
     int_nodeidlist.remove(NODEID)
-
-    # Check if the current nodeid is either the highest or lowest valu
-    if is_outermost_val(NODEID, int_nodeidlist):
-        predecessor, successor = max(int_nodeidlist), min(int_nodeidlist)
-    else:
-        lowerbound_index, upperbound_index = boundaries(int_nodeidlist, NODEID)
-        predecessor=int_nodeidlist[lowerbound_index]
-        successor=int_nodeidlist[upperbound_index]
 
     global NODEID_LIST
     NODEID_LIST=int_nodeidlist
 
-    print(f"{NODEID} my queue is ->", NODEID_LIST)
-
     global PREDECESSOR
-    PREDECESSOR=predecessor
-
     global SUCCESSOR
-    SUCCESSOR=successor
+    # Executes this routine only if node is not the only one in DHT
+    if int_nodeidlist:
+        # Check if the current nodeid is either the highest or lowest valu
+        if is_outermost_val(NODEID, int_nodeidlist):
+            predecessor, successor = max(int_nodeidlist), min(int_nodeidlist)
+        else:
+            lowerbound_index, upperbound_index = boundaries(int_nodeidlist, NODEID)
+            predecessor=int_nodeidlist[lowerbound_index]
+            successor=int_nodeidlist[upperbound_index]
 
-    ch.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        PREDECESSOR=predecessor
+        SUCCESSOR=successor
+    else:
+        PREDECESSOR=NODEID
+        SUCCESSOR=NODEID
 
-    # ch.start_consuming()
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
+# Calculate responsibility interval
+def accept_join(ch, method):
+    ch.stop_consuming()
+    queue_name = QUEUE_NAME
+
+    prepare_join(ch, queue_name)
 
 # def accept_leave():
 
@@ -100,11 +106,10 @@ def callback(ch, method, properties, body):
     if 'new' in body:
         request_join()
     elif 'join' in body:
-        print('this mf wants to join', end=' ')
-        print(body)
         accept_join(ch, method)
     elif 'show' in body:
         print(NODEID_LIST)
+
 
 def rabbit_connect(nodeid):
     # message that will be sent to broker
@@ -128,52 +133,7 @@ def rabbit_connect(nodeid):
     
     time.sleep(1)
     
-    message = f"{nodeid}"
-    # publishes the message to all queues (routing_key='')
-    channel.basic_publish(exchange='logs', routing_key='', body=message)
-    
-    time.sleep(1)
-
-    ########## DONE. BELOW IS THE ROUTINE WHERE WE RETRIEVE THE NODEIDS FROM THE MESSAGE QUEUE #########
-
-    # Get all messages until the queue is empty
-    nodeidlist=[]
-    while True:
-        mf,pr,body=channel.basic_get(queue=queue_name, auto_ack=True)
-        if mf:
-            print(f"{nodeid} read from it's queue -> {body}")
-            nodeidlist.append(body)
-        else:
-            break
-
-    # Convert the elements in the list to int
-    int_nodeidlist=list( map(lambda x: int(x), nodeidlist) )
-
-    # Remove it's own nodeid from list
-    int_nodeidlist.remove(NODEID)
-
-    global NODEID_LIST
-    NODEID_LIST=int_nodeidlist
-
-    print(f"{NODEID} my queue is ->", NODEID_LIST)
-
-    # Executes this routine only if node is not the only one in DHT
-    if int_nodeidlist:
-        # Check if the current nodeid is either the highest or lowest valu
-        if is_outermost_val(NODEID, int_nodeidlist):
-            predecessor, successor = max(int_nodeidlist), min(int_nodeidlist)
-        else:
-            lowerbound_index, upperbound_index = boundaries(int_nodeidlist, NODEID)
-            predecessor=int_nodeidlist[lowerbound_index]
-            successor=int_nodeidlist[upperbound_index]
-
-        global PREDECESSOR
-        PREDECESSOR=predecessor
-
-        global SUCCESSOR
-        SUCCESSOR=successor
-
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    prepare_join(channel, queue_name)
 
     channel.start_consuming()
 
@@ -191,16 +151,9 @@ def request_join(ch, method, properties, body):
 
     NODEID = allocate_new_nodeid()
 
-    print(f"New id joining in DHT: {NODEID}")
-
-    print(f'Recieved {body}')
-    # gerenates a random id
-    # join()
     if (os.fork() == 0):
         AM_CHILD = True
         rabbit_connect(NODEID)
-
-        # join(NODEID)
 
         print(AM_CHILD)
     else:
